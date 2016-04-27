8d69f3432b4955235dce8a2accf79cbf6528b096
Mark Mazumder <mark@markmaz.com>
2016-04-27 15:11:47 -0400

    kickoff pid control

--- server/BotApi.hs
+++ server/BotApi.hs
@@ -1,20 +1,28 @@
-{-# LANGUAGE DataKinds, OverloadedStrings, TypeOperators, RankNTypes, RecordWildCards, ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds, OverloadedStrings, TypeOperators, RankNTypes, DeriveGeneric, RecordWildCards, ScopedTypeVariables, FlexibleContexts #-}
 module BotApi where
 import           Control.Monad.Trans.Either (EitherT)
 import           Control.Monad.Trans.Reader (ReaderT, ask)
 import           Network.Wai (Application)
 import           Network.Wai.Handler.Warp (run)
 import           Control.Monad
+import           Data.Monoid
 import           Control.Concurrent.Async
 import           Control.Concurrent.STM
 import           Control.Monad.IO.Class (liftIO)
 import           Servant
 import           Data.Word
+import           Data.Int
 import           System.Directory
 import           System.Hardware.Serialport
 import qualified Data.List as L
 import qualified Data.Char as C
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Builder as BB
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Aeson as Aeson
+-- import           Data.Aeson.Types (Parser, defaultOptions, fieldLabelModifier)
+import           GHC.Generics
+import           PID
 
 type CommandChan = B.ByteString
 type SerialChan = String
@@ -23,24 +31,55 @@ data BotChan = BotChan {
 , serial :: TChan SerialChan
 }
 
+{-
+dropParse :: (Generic a, Aeson.GFromJSON (Rep a)) => Aeson.Value -> Parser a
+dropParse = Aeson.genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
+dropTo :: (Generic a, Aeson.GToJSON (Rep a)) => a -> Aeson.Value
+dropTo = Aeson.genericToJSON defaultOptions { fieldLabelModifier = drop 1 }
+data ViconLoc = ViconLoc {
+  _xvl :: !Int --TODO(MAZUMDER) Word8
+, _yvl :: !Int
+} deriving (Eq, Show, Generic)
+instance Aeson.FromJSON ViconLoc where
+  parseJSON = dropParse
+instance Aeson.ToJSON ViconLoc where
+  toJSON = dropTo
+-}
+data ViconLoc = ViconLoc {
+  xvl :: !Int --TODO(MAZUMDER) Word8
+, yvl :: !Int
+} deriving (Eq, Show, Generic)
+instance Aeson.FromJSON ViconLoc
+instance Aeson.ToJSON ViconLoc
+
 type BotAPI =     "forward" :> Get '[] ()
             :<|>  "back" :> Get '[] ()
             :<|>  "cw" :> Get '[] ()
             :<|>  "ccw" :> Get '[] ()
             :<|>  "stop" :> Get '[] ()
+            -- :<|>  "location" :> ReqBody '[JSON] ViconLoc :> Post '[JSON] ()
+            :<|>  "location" :> Capture "y" Int :> Get '[] ()
+            :<|>  "cmps" :> Capture "speed" Int :> Get '[] ()
+            :<|>  "rotaterate" :> Capture "r" Double :> Get '[] ()
+            :<|>  "rotate" :> Capture "r" Double :> Get '[] ()
             :<|>  "debug" :> Get '[JSON] [FilePath]
             :<|>  "connect" :> Capture "serial" String :> Get '[] ()
             :<|> Raw
 
-app :: BotChan -> Application
-app bc = serve (Proxy :: Proxy BotAPI) (botapi bc)
+app :: (Double -> IO Double) -> BotChan -> Application
+app pid bc = serve (Proxy :: Proxy BotAPI) (botapi pid bc)
 
-botapi :: BotChan -> ServerT BotAPI (EitherT ServantErr IO)
-botapi bc =    store continuouslyforward
+botapi :: (Double -> IO Double) -> BotChan -> ServerT BotAPI (EitherT ServantErr IO)
+botapi pid bc =    store continuouslyforward
           :<|> store continuouslyback
           :<|> store continuouslycw
           :<|> store continuouslyccw
           :<|> store stop
+          -- :<|> enter (runReaderTNat bc) locationHandler
+          :<|> enter (runReaderTNat bc) locationHandler pid
+          :<|> enter (runReaderTNat bc) cmpsHandler
+          :<|> enter (runReaderTNat bc) rotateRateHandler
+          :<|> enter (runReaderTNat bc) rotateHandler pid
           :<|> debugDevDir
           :<|> connect bc
           :<|> serveDirectory "app.jsexe"
@@ -67,6 +106,72 @@ continuouslyback = queueCommand $ mkDrive [255, 106, 128, 0] -- -15cm/s translat
 stop = queueCommand $ mkDrive [0, 0, 0, 0] -- TODO(MAZUMDER) this does not match with pycreate
 continuouslycw = queueCommand $ mkDrive [0, 67, 255, 255] --  -30 deg/sec rotational
 continuouslyccw = queueCommand $ mkDrive [0, 67, 0, 1] --  +30 deg/sec rotational
+
+
+encodeSpeed :: Int -> BB.Builder
+encodeSpeed cmSpeed = BB.int16BE mmSpeed
+  where mmSpeed = fromIntegral (cmSpeed * 10)
+
+encodeMmSec :: Int -> BB.Builder
+encodeMmSec = BB.int16BE . fromIntegral
+
+radToCommand :: Double -> IO B.ByteString
+radToCommand radPerSec
+  -- | speed < -20 || speed > 20 = print (show speed ++ " too fast") >> stopcmd
+  | velMmSec < -2000 = encNeg (-2000)
+  | velMmSec >  2000 = encPos   2000
+  | velMmSec <     0 = encNeg velMmSec
+  | velMmSec >     0 = encPos velMmSec
+  | velMmSec == 0 = stopcmd
+  -- | otherwise = enc velMmSec
+  where stopcmd = return $ mkDrive [0, 0, 0, 0]
+        encPos s = return . LBS.toStrict . BB.toLazyByteString $ BB.word8 driveOpcode <> encodeMmSec s <> BB.word16BE 0x0001
+        encNeg s = return . LBS.toStrict . BB.toLazyByteString $ BB.word8 driveOpcode <> encodeMmSec s <> BB.word16BE 0xFFFF
+        velMmSec :: Int = round (abs radPerSec * (258.0/2.0))
+          -- compute the velocity, given that the robot's radius is 258mm/2.0
+
+
+cmToCommand :: Int -> IO B.ByteString
+cmToCommand speed
+  -- | speed < -20 || speed > 20 = print (show speed ++ " too fast") >> stopcmd
+  | speed < -50 = enc (-50)
+  | speed >  50 = enc   50
+  | speed == 0 = stopcmd
+  | otherwise = enc speed
+  where stopcmd = return $ mkDrive [0, 0, 0, 0]
+        enc s = return . LBS.toStrict . BB.toLazyByteString $ BB.word8 driveOpcode <> encodeSpeed s <> BB.word16BE 32768
+
+cmpsHandler :: Int -> ReaderT BotChan (EitherT ServantErr IO) ()
+cmpsHandler speed = do
+  cmd <- liftIO $ cmToCommand speed
+  queueCommand cmd
+
+rotateRateHandler :: Double -> ReaderT BotChan (EitherT ServantErr IO) ()
+rotateRateHandler radPerSec = do
+  liftIO $ print radPerSec
+  liftIO $ print $ round (radPerSec * (258.0/2.0))
+  cmd <- liftIO $ radToCommand radPerSec
+  queueCommand cmd
+
+rotateHandler :: (Double -> IO Double) -> Double -> ReaderT BotChan (EitherT ServantErr IO) ()
+rotateHandler pid r = do
+  liftIO $ print ("error: " ++ show r)
+  controlOut <- liftIO $ pid r
+  liftIO $ print ("control: " ++ show controlOut)
+  cmd <- liftIO $ radToCommand controlOut
+  queueCommand cmd
+
+-- locationHandler :: ViconLoc -> ReaderT BotChan (EitherT ServantErr IO) ()
+-- locationHandler v@ViconLoc{..} = do
+--   liftIO $ print v
+locationHandler :: (Double -> IO Double) -> Int -> ReaderT BotChan (EitherT ServantErr IO) ()
+locationHandler pid y = do
+  -- let pid :: _ = givesetpoint 0
+  liftIO $ print ("vicon: " ++ show y)
+  controlOut <- liftIO $ pid (fromIntegral (-y))
+  liftIO $ print ("control: " ++ show controlOut)
+  cmd <- liftIO $ cmToCommand (round controlOut)
+  queueCommand cmd
 
 startOpcode, safeOpcode, driveOpcode :: Word8
 startOpcode = 128
@@ -110,4 +215,5 @@ main = do
   ser <- atomically newTChan
   let botchan = BotChan cmds ser
   _ <- async $ serialx botchan -- TODO(MAZUMDER) clean teardown with withAsync
-  run 9876 $ app botchan
+  pid <- pidTimedIO 0.5 0 0
+  run 9876 $ app (pid 0) botchan
